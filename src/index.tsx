4e7e2bb6635ca260bf50c64922119d0b2203a6d3
Victor Barua <victor.barua@gmail.com>
2020-11-25 14:17:15 -0800

    Configured Tailwind CSS framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import './style';
+import 'tailwindcss/tailwind.css';
 import { FunctionalComponent, h } from 'preact';
 
 const App: FunctionalComponent = () => {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <div class="p-2">
+            <h1 class="text-center text-5xl font-bold">
+                Hello, World!
+            </h1>
+            <div class="pt-2">
+                <button
+                    class="
+                        bg-blue-400 hover:bg-red-700
+                        justify-center
+                        rounded-lg shadow-md
+                        py-2 px-4 te"
+                >Say Hello</button>
+            </div>
+       </div>
     );
 }
 
