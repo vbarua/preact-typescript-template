f571327d1660323c9217221544708a46b7cdda2a
Victor Barua <victor.barua@gmail.com>
2020-11-25 11:00:42 -0800

    Basic Hello World w/ CSS

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import './style';
+import { FunctionalComponent, h } from 'preact';
+
+const App: FunctionalComponent = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default App;
